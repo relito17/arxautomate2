6bd8e04107013030074cbae86eac816ffd414793
Ângelo Rodrigo Sousa Bastos <82399046+relito17@users.noreply.github.com>
2025-03-17 01:19:05 +0000

    commit 34-routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './styles/base.css';
 import './styles/animations.css';
@@ -8,8 +9,17 @@ import './styles/components/services.css';
 import './styles/components/process.css';
 import './styles/components/contact.css';
 
+// Corrige a URL se for um redirecionamento do GitHub Pages
+const urlParams = new URLSearchParams(window.location.search);
+const redirectPath = urlParams.get("redirect");
+if (redirectPath) {
+  window.history.replaceState(null, "", redirectPath);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />   
+    <BrowserRouter>
+      <App />   
+    </BrowserRouter>
   </StrictMode>
 );
